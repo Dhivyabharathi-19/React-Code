a29c82b4d00e07bc5d69e38b58a6a7f788cc9108
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2026-01-25 19:36:17 +0530

    Props

---
+++ Props/props.js
@@ -0,0 +1,10 @@
+function Welcome(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
+function App() {
+  return <Welcome name="Himashini" />;
+}
+
+const container = document.querySelector(".js-container");
+ReactDOM.createRoot(container).render(<App />);
